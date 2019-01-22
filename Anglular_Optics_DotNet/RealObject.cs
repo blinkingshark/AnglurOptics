234b8bd367aad8d4ceff1a77b5684b68b70d6453
steamCrank <darshanamarapana@gmail.com>
2019-01-22 23:35:34 +1300

    Add RealObject class

---
+++ Anglular_Optics_DotNet/RealObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anglular_Optics_DotNet
+{
+    class RealObject
+    {
+        public Double objectHight;
+        public Double oX;
+        public Double oY;
+
+
+        public Double ObjectHeight
+        {
+
+            get { return objectHight; }
+            set { objectHight = value; }
+        }
+    }
+}
